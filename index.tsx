8ba540b1b84c5aac852b8a82436ddad4c7bef796
r80gustavo-coder <r80gustavo@gmail.com>
2025-11-26 23:56:01 -0300

    feat: Update ErrorBoundary to use Component class
    Refactors the `ErrorBoundary` component to explicitly extend `React.Component`, improving type safety and clarity.

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, ErrorInfo } from 'react';
+import React, { ReactNode, ErrorInfo, Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -11,7 +11,7 @@ interface ErrorBoundaryState {
   error: Error | null;
 }
 
-class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   public state: ErrorBoundaryState = {
     hasError: false,
     error: null
